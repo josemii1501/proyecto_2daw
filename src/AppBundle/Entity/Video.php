58b335503bbb4490cdd1376596dc3e3f6ba5a8f8
josemii1501 <josemiguel.lorenzolara1501@gmail.com>
2019-04-23 18:19:11 +0200

    Añadidas Anotaciones de Entidad Video

--- src/AppBundle/Entity/Video.php
+++ src/AppBundle/Entity/Video.php
@@ -12,18 +12,41 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Video
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $route;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $title;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $description;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
     private $date;
 
+    /**
+     * @ORM\Column(type="integer")
+     */
     private $reproductions;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $miniature;
 
 }
