877726d36f887f22d8a304a08b5bccbc8eb14c70
josemii1501 <josemiguel.lorenzolara1501@gmail.com>
2019-04-23 18:24:25 +0200

    Añadidas Anotaciones de Entidad File

--- src/AppBundle/Entity/File.php
+++ src/AppBundle/Entity/File.php
@@ -12,10 +12,21 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class File
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $file;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
     private $date;
 
 }
