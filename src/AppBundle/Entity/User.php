5768da0033d9a5f4202edce8362016d9785ed226
josemii1501 <josemiguel.lorenzolara1501@gmail.com>
2019-04-23 18:25:59 +0200

    Añadidas Anotaciones de Entidad User

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -12,29 +12,70 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class User
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $name;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $lastname;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $email;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $phone;
 
+    /**
+     * @ORM\Column(type="date")
+     */
     private $birthday;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $login;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $password;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $avatar;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $urlWebSite;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $description;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
     private $publisher;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
     private $admin;
 }
