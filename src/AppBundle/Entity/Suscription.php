27ab37af5e71f05e359355b73ea0e10981d3d8b9
josemii1501 <josemiguel.lorenzolara1501@gmail.com>
2019-05-05 16:22:16 +0200

    Suscripcion implementada

--- src/AppBundle/Entity/Suscription.php
+++ src/AppBundle/Entity/Suscription.php
@@ -21,11 +21,82 @@ class Suscription
     private $id;
 
     /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $timestamp;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @var User
+     */
+    private $suscriptor;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @var User
+     */
+    private $chanel;
+    /**
      * @return int
      */
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * @param \DateTime $timestamp
+     * @return Suscription
+     */
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = $timestamp;
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getSuscriptor()
+    {
+        return $this->suscriptor;
+    }
+
+    /**
+     * @param User $suscriptor
+     * @return Suscription
+     */
+    public function setSuscriptor($suscriptor)
+    {
+        $this->suscriptor = $suscriptor;
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getChanel()
+    {
+        return $this->chanel;
+    }
+
+    /**
+     * @param User $chanel
+     * @return Suscription
+     */
+    public function setChanel($chanel)
+    {
+        $this->chanel = $chanel;
+        return $this;
     }
 
 }
